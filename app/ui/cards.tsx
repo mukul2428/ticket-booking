8a1101ddf0e335b21a7a8270b19d33f145c8d87a
Harshdeep Bhati <harshdeep.rana.377@gmail.com>
2024-01-06 00:09:02 +0530

    redirection done from cards

--- app/ui/cards.tsx
+++ app/ui/cards.tsx
@@ -1,31 +1,33 @@
 import React, { useContext } from "react";
 import { EventContext } from "../hooks/context";
-
+import Link from "next/link";
 const Cards = () => {
   const eventsData = useContext(EventContext);
   return (
     <>
-      <div className="relative max-w-sm shadow-custom rounded-lg shadow ">
-        <img
-          className="rounded-t-lg object-cover h-48 w-96"
-          src={eventsData.imageType}
-          alt=""
-        />
+      <Link href={"/sell-tickets/ticket-details"}>
+        <div className="relative max-w-sm shadow-custom rounded-lg shadow ">
+          <img
+            className="rounded-t-lg object-cover h-48 w-96"
+            src={eventsData.imageType}
+            alt=""
+          />
 
-        <div className="p-5">
-          <span className="p-2 rounded-md text-white bg-orange-600">
-            {eventsData.eventCategory}
-          </span>
-          <h5 className="dark:text-black mb-2 text-2xl font-bold tracking-tight text-gray-900">
-            {eventsData.eventName}
-          </h5>
+          <div className="p-5">
+            <span className="p-2 rounded-md text-white bg-orange-600">
+              {eventsData.eventCategory}
+            </span>
+            <h5 className="dark:text-black mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              {eventsData.eventName}
+            </h5>
 
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
--- app/ui/homeImg.tsx
+++ app/ui/homeImg.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { EventContext } from "../hooks/context";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
+import Link from "next/link";
 
 const HomeImg = () => {
   const loginClicked = useSelector((state: RootState) => state.loginClicked);
